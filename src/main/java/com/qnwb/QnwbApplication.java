0eea7766ebbb130fa8c935896ef2616ad4aafc1c
BreezeOfSummerNight <17601606210@163.com>
2021-01-27 17:13:55 +0800

    Initial commit

---
+++ src/main/java/com/qnwb/QnwbApplication.java
@@ -0,0 +1,13 @@
+package com.qnwb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnwbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QnwbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qnwb/QnwbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qnwb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnwbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
